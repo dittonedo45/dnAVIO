8d7526473a032c8a679afc3342efabe22e0ec42b
Eddington Chamunorwa <dittonedo45@github.com>
2021-12-05 11:49:30 +0200

    - Adding License.

--- plugin/c/vavio.c
+++ plugin/c/vavio.c
@@ -1,4 +1,25 @@
 /*XXX This Document was modified on 1634629006 */
+/*
+ * Copyright (c) 2021 Eddington Chamunorwa <dittonedo45@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,262 +29,264 @@
 
 static jv *main_log = 0;
 
-static void dn_set_opts ( AVDictionary ** opts, jv dict )
+static void dn_set_opts(AVDictionary ** opts, jv dict)
 {
- char *key = 0, *val = 0;
- jv_object_foreach ( jv_copy ( dict ), vkey, vval ) {
-  if( jv_get_kind ( vval ) == JV_KIND_STRING ) {
-   val = ( char * )jv_string_value ( vval );
-   key = ( char * )jv_string_value ( vkey );
-   ( void )av_dict_set ( opts, key, val, AV_OPT_SEARCH_CHILDREN );
-  }
- }
+    char *key = 0, *val = 0;
+    jv_object_foreach(jv_copy(dict), vkey, vval) {
+	if (jv_get_kind(vval) == JV_KIND_STRING) {
+	    val = (char *) jv_string_value(vval);
+	    key = (char *) jv_string_value(vkey);
+	    (void) av_dict_set(opts, key, val, AV_OPT_SEARCH_CHILDREN);
+	}
+    }
 }
 
-static void dn_jv_add ( jv * ob, char *key, jv el )
+static void dn_jv_add(jv * ob, char *key, jv el)
 {
- if( ob && jv_get_kind ( *ob ) == JV_KIND_OBJECT ) {
-  *ob = jv_object_set ( *ob, jv_string ( key ), el );
- } else if( ob && jv_get_kind ( *ob ) == JV_KIND_ARRAY ) {
-  *ob = jv_array_append ( *ob, el );
- }
+    if (ob && jv_get_kind(*ob) == JV_KIND_OBJECT) {
+	*ob = jv_object_set(*ob, jv_string(key), el);
+    } else if (ob && jv_get_kind(*ob) == JV_KIND_ARRAY) {
+	*ob = jv_array_append(*ob, el);
+    }
 }
 
-static char *dn_jv_ans ( jv o )
+static char *dn_jv_ans(jv o)
 {
- return strdup ( jv_string_value ( jv_dump_string ( o, 0 ) ) );
+    return strdup(jv_string_value(jv_dump_string(o, 0)));
 }
 
-static void log_callback ( void *opa, int t, const char *str, va_list arg )
+static void log_callback(void *opa, int t, const char *str, va_list arg)
 {
- char *exp = 0;
- vasprintf ( &exp, str, arg );
- {
-  jv data = jv_object (  );
-  dn_jv_add ( &data, "type", jv_number ( t ) );
-  dn_jv_add ( &data, "issue", jv_string ( exp ) );
-  dn_jv_add ( ( jv * ) main_log, NULL, data );
- }
- free ( exp );
+    char *exp = 0;
+    vasprintf(&exp, str, arg);
+    {
+	jv data = jv_object();
+	dn_jv_add(&data, "type", jv_number(t));
+	dn_jv_add(&data, "issue", jv_string(exp));
+	dn_jv_add((jv *) main_log, NULL, data);
+    }
+    free(exp);
 }
 
-char *avio_url ( char *url )
+char *avio_url(char *url)
 {
- AVIOContext *avio = 0;
- AVDictionary *opts = 0;
- jv ans = jv_object (  );
- jv log = jv_array (  );
- jv mi_info = jv_object (  );
+    AVIOContext *avio = 0;
+    AVDictionary *opts = 0;
+    jv ans = jv_object();
+    jv log = jv_array();
+    jv mi_info = jv_object();
 
- int ret = 0;
- char *op = 0;
- char *rul = 0;
+    int ret = 0;
+    char *op = 0;
+    char *rul = 0;
 
- if( !url || !strlen ( url ) ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "url is empty" ) );
-  dn_jv_add ( &ans, "contents", jv_array (  ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (!url || !strlen(url)) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("url is empty"));
+	dn_jv_add(&ans, "contents", jv_array());
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- jv obj = jv_parse ( url );
+    jv obj = jv_parse(url);
 
- if( jv_get_kind ( obj ) != JV_KIND_OBJECT ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error",
-              jv_string ( "The input Json is not properly structured" ) );
-  dn_jv_add ( &ans, "contents", jv_array (  ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (jv_get_kind(obj) != JV_KIND_OBJECT) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error",
+		  jv_string("The input Json is not properly structured"));
+	dn_jv_add(&ans, "contents", jv_array());
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- if( jv_object_has ( jv_copy ( obj ), jv_string ( "url" ) ) ) {
-  jv t = jv_object_get ( jv_copy ( obj ), jv_string ( "url" ) );
-  if( jv_get_kind ( t ) == JV_KIND_STRING ) {
-   rul = strdup ( ( char * )jv_string_value ( t ) );
-  }
- }
+    if (jv_object_has(jv_copy(obj), jv_string("url"))) {
+	jv t = jv_object_get(jv_copy(obj), jv_string("url"));
+	if (jv_get_kind(t) == JV_KIND_STRING) {
+	    rul = strdup((char *) jv_string_value(t));
+	}
+    }
 
- if( !rul || !strlen ( rul ) ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "url is empty" ) );
-  dn_jv_add ( &ans, "contents", jv_array (  ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (!rul || !strlen(rul)) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("url is empty"));
+	dn_jv_add(&ans, "contents", jv_array());
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- if( jv_object_has ( jv_copy ( obj ), jv_string ( "options" ) ) ) {
-  jv t = jv_object_get ( jv_copy ( obj ), jv_string ( "options" ) );
-  if( jv_get_kind ( t ) == JV_KIND_OBJECT ) {
-   dn_set_opts ( &opts, t );
-  }
- }
+    if (jv_object_has(jv_copy(obj), jv_string("options"))) {
+	jv t = jv_object_get(jv_copy(obj), jv_string("options"));
+	if (jv_get_kind(t) == JV_KIND_OBJECT) {
+	    dn_set_opts(&opts, t);
+	}
+    }
 
- main_log = &log;
- av_log_set_callback ( log_callback );
+    main_log = &log;
+    av_log_set_callback(log_callback);
 
- ret = avio_open2 ( &avio, rul, AVIO_FLAG_READ, NULL, &opts );
+    ret = avio_open2(&avio, rul, AVIO_FLAG_READ, NULL, &opts);
 
- if( ret < 0 ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( av_err2str ( ret ) ) );
-  dn_jv_add ( &ans, "contents", jv_array (  ) );
-  dn_jv_add ( &ans, "logs", log );
-  avio_close ( avio );
-  return dn_jv_ans ( ans );
- }
+    if (ret < 0) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string(av_err2str(ret)));
+	dn_jv_add(&ans, "contents", jv_array());
+	dn_jv_add(&ans, "logs", log);
+	avio_close(avio);
+	return dn_jv_ans(ans);
+    }
 
- char *buf = malloc ( 1 );
- int i = 0;
+    char *buf = malloc(1);
+    int i = 0;
 
- while( 1 ) {
-  char bbuf[1054];
-  ret = avio_read ( avio, bbuf, sizeof ( bbuf ) );
-  if( ret <= 0 )
-   break;
+    while (1) {
+	char bbuf[1054];
+	ret = avio_read(avio, bbuf, sizeof(bbuf));
 
-  buf = realloc ( buf, ( i + ret ) * sizeof ( char ) );
-  strncpy ( buf + i, bbuf, ret );
-  i += ret;
- }
+	if (ret <= 0)
+	    break;
 
- avio_close ( avio );
+	buf = realloc(buf, (i + ret) * sizeof(char));
+	strncpy(buf + i, bbuf, ret);
+	i += ret;
+    }
 
- if( i == 0 ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "no data received" ) );
-  dn_jv_add ( &ans, "contents", jv_array (  ) );
-  dn_jv_add ( &ans, "logs", log );
-  free ( buf );
-  return 0;
- }
+    avio_close(avio);
 
- buf[i] = 0;
- {
-  jv cnts = jv_array (  );
-  char *p = buf;
-  while( p && *p ) {
-   char *t = strchr ( p, '\n' );
-   if( t ) {
-	dn_jv_add ( &cnts, 0, jv_string_fmt ( "%.*s", t - p, p ) );
-   } else {
-	dn_jv_add ( &cnts, 0, jv_string_fmt ( "%s", p ) );
-	break;
-   }
-   p = t + 1;
-  }
-  dn_jv_add ( &ans, "valid", jv_true (  ) );
-  dn_jv_add ( &ans, "error", jv_null (  ) );
-  dn_jv_add ( &ans, "contents", cnts );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (i == 0) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("no data received"));
+	dn_jv_add(&ans, "contents", jv_array());
+	dn_jv_add(&ans, "logs", log);
+	free(buf);
+	return 0;
+    }
+
+    buf[i] = 0;
+    {
+	jv cnts = jv_array();
+	char *p = buf;
+	while (p && *p) {
+	    char *t = strchr(p, '\n');
+	    if (t) {
+		dn_jv_add(&cnts, 0, jv_string_fmt("%.*s", t - p, p));
+	    } else {
+		dn_jv_add(&cnts, 0, jv_string_fmt("%s", p));
+		break;
+	    }
+	    p = t + 1;
+	}
+	dn_jv_add(&ans, "valid", jv_true());
+	dn_jv_add(&ans, "error", jv_null());
+	dn_jv_add(&ans, "contents", cnts);
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 }
 
-char *avio_url_write ( char *url )
+char *avio_url_write(char *url)
 {
- AVIOContext *avio = 0;
- AVDictionary *opts = 0;
- jv ans = jv_object (  );
- jv mi_info = jv_object (  );
- jv log = jv_array (  );
- jv contents = jv_null (  );
+    AVIOContext *avio = 0;
+    AVDictionary *opts = 0;
+    jv ans = jv_object();
+    jv mi_info = jv_object();
+    jv log = jv_array();
+    jv contents = jv_null();
 
- int ret = 0;
- char *op = 0;
- char *rul = 0;
+    int ret = 0;
+    char *op = 0;
+    char *rul = 0;
 
- if( !url || !strlen ( url ) ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "url is empty" ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (!url || !strlen(url)) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("url is empty"));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- jv obj = jv_parse ( url );
+    jv obj = jv_parse(url);
 
- if( jv_get_kind ( obj ) != JV_KIND_OBJECT ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error",
-              jv_string ( "The input Json is not properly structured" ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (jv_get_kind(obj) != JV_KIND_OBJECT) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error",
+		  jv_string("The input Json is not properly structured"));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- if( jv_object_has ( jv_copy ( obj ), jv_string ( "url" ) ) ) {
-  jv t = jv_object_get ( jv_copy ( obj ), jv_string ( "url" ) );
-  if( jv_get_kind ( t ) == JV_KIND_STRING ) {
-   rul = strdup ( ( char * )jv_string_value ( t ) );
-  }
- }
+    if (jv_object_has(jv_copy(obj), jv_string("url"))) {
+	jv t = jv_object_get(jv_copy(obj), jv_string("url"));
+	if (jv_get_kind(t) == JV_KIND_STRING) {
+	    rul = strdup((char *) jv_string_value(t));
+	}
+    }
 
- if( !rul || !strlen ( rul ) ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "url is empty" ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (!rul || !strlen(rul)) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("url is empty"));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- if( jv_object_has ( jv_copy ( obj ), jv_string ( "options" ) ) ) {
-  jv t = jv_object_get ( jv_copy ( obj ), jv_string ( "options" ) );
-  if( jv_get_kind ( t ) == JV_KIND_OBJECT ) {
-   dn_set_opts ( &opts, t );
-  }
- }
+    if (jv_object_has(jv_copy(obj), jv_string("options"))) {
+	jv t = jv_object_get(jv_copy(obj), jv_string("options"));
+	if (jv_get_kind(t) == JV_KIND_OBJECT) {
+	    dn_set_opts(&opts, t);
+	}
+    }
 
- if( jv_object_has ( jv_copy ( obj ), jv_string ( "contents" ) ) ) {
-  contents = jv_object_get ( jv_copy ( obj ), jv_string ( "contents" ) );
- }
+    if (jv_object_has(jv_copy(obj), jv_string("contents"))) {
+	contents = jv_object_get(jv_copy(obj), jv_string("contents"));
+    }
 
- if( jv_get_kind ( contents ) != JV_KIND_ARRAY ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "contents are not in array form." ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (jv_get_kind(contents) != JV_KIND_ARRAY) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error",
+		  jv_string("contents are not in array form."));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- if( jv_array_length ( jv_copy ( contents ) ) == 0 ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( "contents array is empty" ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
- main_log = &log;
- av_log_set_level ( AV_LOG_VERBOSE | AV_LOG_DEBUG );
- av_log_set_callback ( log_callback );
- ret = avio_open2 ( &avio, rul, AVIO_FLAG_WRITE, NULL, &opts );
+    if (jv_array_length(jv_copy(contents)) == 0) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string("contents array is empty"));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
+    main_log = &log;
+    av_log_set_level(AV_LOG_VERBOSE | AV_LOG_DEBUG);
+    av_log_set_callback(log_callback);
+    ret = avio_open2(&avio, rul, AVIO_FLAG_WRITE, NULL, &opts);
 
- if( ret < 0 ) {
-  dn_jv_add ( &ans, "valid", jv_false (  ) );
-  dn_jv_add ( &ans, "error", jv_string ( av_err2str ( ret ) ) );
-  dn_jv_add ( &ans, "written", jv_number ( 0 ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    if (ret < 0) {
+	dn_jv_add(&ans, "valid", jv_false());
+	dn_jv_add(&ans, "error", jv_string(av_err2str(ret)));
+	dn_jv_add(&ans, "written", jv_number(0));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 
- int chunks_written = 0;
- jv_array_foreach ( jv_copy ( contents ), iter_i, vall ) {
-  char *buf = 0;
-  asprintf ( &buf, "%s\n", jv_string_value ( vall ) );
-  int r = 0;
-  avio_write ( avio, buf, ( r = strlen ( buf ) ) );
-  chunks_written += r;
-  free ( buf );
- }
+    int chunks_written = 0;
+    jv_array_foreach(jv_copy(contents), iter_i, vall) {
+	char *buf = 0;
+	asprintf(&buf, "%s\n", jv_string_value(vall));
+	int r = 0;
+	avio_write(avio, buf, (r = strlen(buf)));
+	chunks_written += r;
+	free(buf);
+    }
 
- {
-  avio_flush ( avio );
-  avio_close ( avio );
-  dn_jv_add ( &ans, "valid", jv_true (  ) );
-  dn_jv_add ( &ans, "error", jv_null (  ) );
-  dn_jv_add ( &ans, "written", jv_number ( chunks_written ) );
-  dn_jv_add ( &ans, "logs", log );
-  return dn_jv_ans ( ans );
- }
+    {
+	avio_flush(avio);
+	avio_close(avio);
+	dn_jv_add(&ans, "valid", jv_true());
+	dn_jv_add(&ans, "error", jv_null());
+	dn_jv_add(&ans, "written", jv_number(chunks_written));
+	dn_jv_add(&ans, "logs", log);
+	return dn_jv_ans(ans);
+    }
 }
